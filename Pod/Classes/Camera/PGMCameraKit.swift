3d88b7ac78a181bbbd585b42c8f97af5ce2bc035
pablogm <invanzert@gmail.com>
2016-04-03 13:30:36 +0200

    * Fixed issue #3 also when recording without sound.

--- Pod/Classes/Camera/PGMCameraKit.swift
+++ Pod/Classes/Camera/PGMCameraKit.swift
@@ -660,8 +660,8 @@ public typealias ImageCompletionType = (UIImage?, NSError?, LocalIdentifierType?
                     
                     self.videoWriter = PGMCameraKitWriter(
                         fileUrl: self.filePathUrl(),
-                        height: AVCaptureVideoOrientation(ui:UIDevice.currentDevice().orientation) == .Portrait ? (self.cameraDevice == .Back ? self.width! : self.height!) : (self.cameraDevice == .Back ? self.height! : self.width!),
-                        width: AVCaptureVideoOrientation(ui:UIDevice.currentDevice().orientation) == .Portrait ? (self.cameraDevice == .Back ? self.height! : self.width!) : (self.cameraDevice == .Back ? self.width! : self.height!)
+                        height: AVCaptureVideoOrientation(ui:UIDevice.currentDevice().orientation) == .Portrait ? (self.cameraDevice == .Back ? self.width! : self.height!) : (self.height!),
+                        width: AVCaptureVideoOrientation(ui:UIDevice.currentDevice().orientation) == .Portrait ? (self.cameraDevice == .Back ? self.height! : self.width!) : (self.width!),
                     )
                 }
             }
